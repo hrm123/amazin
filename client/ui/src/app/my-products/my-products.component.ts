d941a32163193b14d9b039524a13207243b1f299
DESKTOP-EI787M1\Ramm <bestcoder0@yahoo.com>
2018-05-13 11:50:29 -0400

    product paging implemented

--- client/ui/src/app/my-products/my-products.component.ts
+++ client/ui/src/app/my-products/my-products.component.ts
@@ -12,14 +12,30 @@ import {Router} from '@angular/router'
 })
 export class MyProductsComponent implements OnInit {
   products: any;
+  productsCache: any;
+
+  currentPage = 1;
+  pageSize = 20;
+  totalSize = 1;
+
   constructor(private data: DataService, private rest: RestApiService, private router: Router) { }
+
+  loadPage(page: number) {
+    this.currentPage = page;
+    let startIndex = (this.currentPage-1)*this.pageSize;
+    let endIndex = startIndex + this.pageSize;
+    this.products = this.productsCache.slice(startIndex, endIndex);
+  }
 
   async ngOnInit() {
     try {
       const data = await this.rest.get('http://localhost:3030/api/seller/products');
 
       if(data['success'] ) {
-        this.products = data['products'];
+        this.productsCache = data['products'];
+        this.totalSize = Math.ceil(this.productsCache.length / this.pageSize);
+        // this.products = this.productsCache.slice((this.currentPage-1)*this.pageSize, this.pageSize);
+        this.loadPage(1);
         console.log('this.products', this.products);
       } else {
         this.data.error(data['message']);
