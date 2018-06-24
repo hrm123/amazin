be6f214ac332bc5ba444019ebd8c4f3f3ac2ce7b
DESKTOP-EI787M1\Ramm <bestcoder0@yahoo.com>
2018-06-23 20:08:09 -0400

    delta

--- client/ui/src/app/app.module.ts
+++ client/ui/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -48,6 +48,7 @@ import { ChatComponent } from './chat/chat.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     NgbModule.forRoot(),
     FormsModule,
     AppRoutingModule,
